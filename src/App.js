49e71bfae7293369f0d3893a2658efabe4b62ce6
Balaram Sahu <108859384+balaram606@users.noreply.github.com>
2023-07-23 23:02:20 +0530

    project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import InputForm from "./components/inputForm";
-import Introduction from "./components/Introduction";
-import React, { useState } from "react";
 function App() {
-  const [showIntro, setShowIntro] = useState(localStorage["intro"]);
-  const handleNext = () => {
-    localStorage.setItem("intro", "true");
-    setShowIntro(true);
-  };
 
   return (
-    <div>
-      {showIntro?(
-      <div>
-      <InputForm />
-    </div>
-      ):<Introduction next={handleNext}/>}
-    </div>
+    <>
+    <InputForm />
+    </>
   );
 }
-
 export default App;
